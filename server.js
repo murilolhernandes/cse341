6addb956fa1457a39f229c3c8bd1eca08fe6764c
murilolhernandes <murilo.luiz.hernandes@gmail.com>
2026-01-05 14:15:41 -0700

    first commit - started the course

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+
+
+app.get('/', (req, res) => {
+  res.send("Hello");
+});
+
+const port = 8080;
+
+app.listen(process.env.port || port);
+console.log('Web Server is listening at port ' + (process.env.port || port));
